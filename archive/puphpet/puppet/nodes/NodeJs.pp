f7d30c8e327f238ee8c58fa9f69c4903bf8691f2
Juan Treminio <jtreminio@gmail.com>
2015-03-02 00:56:29 -0600

    Updates Nodejs to standards

--- archive/puphpet/puppet/nodes/NodeJs.pp
+++ archive/puphpet/puppet/nodes/NodeJs.pp
@@ -1,11 +1,11 @@
-if $nodejs_values == undef { $nodejs_values = hiera_hash('nodejs', false) }
-
 include puphpet::params
 
 if hash_key_equals($nodejs_values, 'install', 1) {
   include puphpet::nodejs
 
-  if is_array($nodejs_values['npm_packages']) and count($nodejs_values['npm_packages']) > 0 {
+  if is_array($nodejs_values['npm_packages'])
+    and count($nodejs_values['npm_packages']) > 0
+  {
     each( $nodejs_values['npm_packages'] ) |$package| {
         $npm_array = split($package, '@')
 
--- archive/puphpet/puppet/site.pp
+++ archive/puphpet/puppet/site.pp
@@ -1,65 +1,18 @@
-if $yaml_values == undef {
-  $yaml_values = loadyaml('/vagrant/puphpet/config.yaml')
-}
-
-if $apache_values == undef {
-  $apache_values = $yaml_values['apache']
-}
-
-if $beanstalkd_values == undef {
-  $beanstalkd_values = hiera_hash('beanstalkd', false)
-}
-
-if $cron_values == undef {
-  $cron_values = hiera_hash('cron', false)
-}
-
-if $drush_values == undef {
-  $drush_values = hiera_hash('drush', false)
-}
-
-if $elasticsearch_values == undef {
-  $elasticsearch_values = hiera_hash('elastic_search', false)
-}
-
-if $firewall_values == undef {
-  $firewall_values = hiera_hash('firewall', false)
-}
-
-if $hhvm_values == undef {
-  $hhvm_values = hiera_hash('hhvm', false)
-}
-
-if $mailcatcher_values == undef {
-  $mailcatcher_values = hiera_hash('mailcatcher', false)
-}
-
-if $mariadb_values == undef {
-  $mariadb_values = hiera_hash('mariadb', false)
-}
-
-if $mongodb_values == undef {
-  $mongodb_values = hiera_hash('mongodb', false)
-}
-
-if $mysql_values == undef {
-  $mysql_values = hiera_hash('mysql', false)
-}
-
-if $nginx_values == undef {
-  $nginx_values = hiera_hash('nginx', false)
-}
-
-if $php_values == undef {
-  $php_values = hiera_hash('php', false)
-}
-
-if $vm_values == undef {
-  $vm_values = hiera_hash($::vm_target_key, false)
-}
-
-if $yaml_values == undef {
-  $yaml_values = loadyaml('/vagrant/puphpet/config.yaml')
-}
+$apache_values = $yaml_values['apache']
+$beanstalkd_values = hiera_hash('beanstalkd', false)
+$cron_values = hiera_hash('cron', false)
+$drush_values = hiera_hash('drush', false)
+$elasticsearch_values = hiera_hash('elastic_search', false)
+$firewall_values = hiera_hash('firewall', false)
+$hhvm_values = hiera_hash('hhvm', false)
+$mailcatcher_values = hiera_hash('mailcatcher', false)
+$mariadb_values = hiera_hash('mariadb', false)
+$mongodb_values = hiera_hash('mongodb', false)
+$mysql_values = hiera_hash('mysql', false)
+$nginx_values = hiera_hash('nginx', false)
+$nodejs_values = hiera_hash('nodejs', false)
+$php_values = hiera_hash('php', false)
+$vm_values = hiera_hash($::vm_target_key, false)
+$yaml_values = loadyaml('/vagrant/puphpet/config.yaml')
 
 import 'nodes/*.pp'
