939cd5ec526f60b6938bf3132788c25bdffee45f
Juan Treminio <jtreminio@gmail.com>
2015-03-01 23:04:22 -0600

    Updates Beanstalkd to standards

--- archive/puphpet/puppet/site.pp
+++ archive/puphpet/puppet/site.pp
@@ -1,14 +1,25 @@
 if $yaml_values == undef {
   $yaml_values = loadyaml('/vagrant/puphpet/config.yaml')
 }
+
 if $apache_values == undef {
   $apache_values = $yaml_values['apache']
 }
-if $php_values == undef {
-  $php_values = hiera_hash('php', false)
+
+if $beanstalkd_values == undef {
+  $beanstalkd_values = hiera_hash('beanstalkd', false)
 }
+
 if $hhvm_values == undef {
   $hhvm_values = hiera_hash('hhvm', false)
+}
+
+if $nginx_values == undef {
+  $nginx_values = hiera_hash('nginx', false)
+}
+
+if $php_values == undef {
+  $php_values = hiera_hash('php', false)
 }
 
 import 'nodes/*.pp'
