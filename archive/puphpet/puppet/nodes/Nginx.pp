090fd732b343fa35379df09503e44bc70f5fd16c
Juan Treminio <jtreminio@gmail.com>
2015-03-14 00:13:35 -0500

    Refactors proxy support

--- archive/puphpet/puppet/nodes/Nginx.pp
+++ archive/puphpet/puppet/nodes/Nginx.pp
@@ -46,6 +46,28 @@ if hash_key_equals($nginx_values, 'install', 1) {
 
   create_resources('class', { 'nginx' => $nginx_settings })
 
+  $nginx_upstreams = merge({}, $nginx_values['upstreams'])
+
+  each( $nginx_upstreams ) |$key, $upstream| {
+    $upstream_name = $upstream['name']
+
+    $upstream_merged = delete($upstream, ['name'])
+
+    create_resources(nginx::resource::upstream, {
+      "${upstream_name}" => $upstream_merged
+    })
+
+    each( $upstream['members'] ) |$key, $member| {
+      $member_array = $package_array = split($member, ':')
+
+      if count($member_array) == 2
+        and ! defined(Puphpet::Firewall::Port[$member_array[1]])
+      {
+        puphpet::firewall::port { $member_array[1]: }
+      }
+    }
+  }
+
   # Creates a default vhost entry if user chose to do so
   if hash_key_equals($nginx_values['settings'], 'default_vhost', 1) {
     $nginx_vhosts = merge($nginx_values['vhosts'], {
@@ -101,99 +123,97 @@ if hash_key_equals($nginx_values, 'install', 1) {
   }
 
   each( $nginx_vhosts ) |$key, $vhost| {
-    if ! defined($vhost['proxy']) or $vhost['proxy'] == '' {
-      exec { "exec mkdir -p ${vhost['www_root']} @ key ${key}":
-        command => "mkdir -p ${vhost['www_root']}",
-        user    => $webroot_user,
-        group   => $webroot_group,
-        creates => $vhost['www_root'],
-        require => File[$www_location],
+    exec { "exec mkdir -p ${vhost['www_root']} @ key ${key}":
+      command => "mkdir -p ${vhost['www_root']}",
+      user    => $webroot_user,
+      group   => $webroot_group,
+      creates => $vhost['www_root'],
+      require => File[$www_location],
+    }
+
+    if ! defined(File[$vhost['www_root']]) {
+      file { $vhost['www_root']:
+        ensure  => directory,
+        mode    => '0775',
+        require => Exec["exec mkdir -p ${vhost['www_root']} @ key ${key}"],
+      }
+    }
+
+    # the gui passes "server_name" and "server_aliases"
+    # "server_aliases" is not actually in puppet-nginx
+    $server_names = unique(flatten(
+      concat([$vhost['server_name']], $vhost['server_aliases'])
+    ))
+
+    $ssl = array_true($vhost, 'ssl') ? {
+      true    => true,
+      default => false,
+    }
+    $ssl_cert = array_true($vhost, 'ssl_cert') ? {
+      true    => $vhost['ssl_cert'],
+      default => $puphpet::params::ssl_cert_location,
+    }
+    $ssl_key = array_true($vhost, 'ssl_key') ? {
+      true    => $vhost['ssl_key'],
+      default => $puphpet::params::ssl_key_location,
+    }
+    $ssl_port = array_true($vhost, 'ssl_port') ? {
+      true    => $vhost['ssl_port'],
+      default => '443',
+    }
+    $rewrite_to_https = array_true($vhost, 'rewrite_to_https') ? {
+      true    => true,
+      default => false,
+    }
+
+    # puppet-nginx is stupidly strict about ssl value datatypes
+    $vhost_merged = delete(merge($vhost, {
+      'server_name'          => $server_names,
+      'use_default_location' => false,
+      'ssl'                  => $ssl,
+      'ssl_cert'             => $ssl_cert,
+      'ssl_key'              => $ssl_key,
+      'ssl_port'             => $ssl_port,
+      'rewrite_to_https'     => $rewrite_to_https,
+    }), ['server_aliases', 'proxy', 'locations'])
+
+    create_resources(nginx::resource::vhost, { "${key}" => $vhost_merged })
+
+    each( $vhost['locations'] ) |$lkey, $location| {
+      # remove empty values
+      $location_trimmed = merge({
+        'fast_cgi_params_extra' => [],
+      }, delete_values($location, ''))
+
+      # Takes gui ENV vars: fastcgi_param {ENV_NAME} {VALUE}
+      $location_custom_cfg_append = prefix(
+        $location_trimmed['fast_cgi_params_extra'],
+        'fastcgi_param '
+      )
+
+      # separate from $location_trimmed because some values
+      # really need to be set to a default.
+      # Removes fast_cgi_params_extra because it only exists in gui
+      # not puppet-nginx
+      $location_no_root = delete(merge({
+        'vhost'                      => $key,
+        'ssl'                        => $ssl,
+        'location_custom_cfg_append' => $location_custom_cfg_append,
+      }, $location_trimmed), 'fast_cgi_params_extra')
+
+      # If www_root was removed with all the trimmings,
+      # add it back it
+      if ! defined($location_no_root['fastcgi'])
+        or empty($location_no_root['fastcgi'])
+      {
+        $location_merged = merge({
+          'www_root' => $vhost['www_root'],
+        }, $location_no_root)
+      } else {
+        $location_merged = $location_no_root
       }
 
-      if ! defined(File[$vhost['www_root']]) {
-        file { $vhost['www_root']:
-          ensure  => directory,
-          mode    => '0775',
-          require => Exec["exec mkdir -p ${vhost['www_root']} @ key ${key}"],
-        }
-      }
-
-      # the gui passes "server_name" and "server_aliases"
-      # "server_aliases" is not actually in puppet-nginx
-      $server_names = unique(flatten(
-        concat([$vhost['server_name']], $vhost['server_aliases'])
-      ))
-
-      $ssl = array_true($vhost, 'ssl') ? {
-        true    => true,
-        default => false,
-      }
-      $ssl_cert = array_true($vhost, 'ssl_cert') ? {
-        true    => $vhost['ssl_cert'],
-        default => $puphpet::params::ssl_cert_location,
-      }
-      $ssl_key = array_true($vhost, 'ssl_key') ? {
-        true    => $vhost['ssl_key'],
-        default => $puphpet::params::ssl_key_location,
-      }
-      $ssl_port = array_true($vhost, 'ssl_port') ? {
-        true    => $vhost['ssl_port'],
-        default => '443',
-      }
-      $rewrite_to_https = array_true($vhost, 'rewrite_to_https') ? {
-        true    => true,
-        default => false,
-      }
-
-      # puppet-nginx is stupidly strict about ssl value datatypes
-      $vhost_merged = delete(merge($vhost, {
-        'server_name'          => $server_names,
-        'use_default_location' => false,
-        'ssl'                  => $ssl,
-        'ssl_cert'             => $ssl_cert,
-        'ssl_key'              => $ssl_key,
-        'ssl_port'             => $ssl_port,
-        'rewrite_to_https'     => $rewrite_to_https,
-      }), ['server_aliases', 'proxy', 'locations'])
-
-      create_resources(nginx::resource::vhost, { "${key}" => $vhost_merged })
-
-      each( $vhost['locations'] ) |$lkey, $location| {
-        # remove empty values
-        $location_trimmed = merge({
-          'fast_cgi_params_extra' => [],
-        }, delete_values($location, ''))
-
-        # Takes gui ENV vars: fastcgi_param {ENV_NAME} {VALUE}
-        $location_custom_cfg_append = prefix(
-          $location_trimmed['fast_cgi_params_extra'],
-          'fastcgi_param '
-        )
-
-        # separate from $location_trimmed because some values
-        # really need to be set to a default.
-        # Removes fast_cgi_params_extra because it only exists in gui
-        # not puppet-nginx
-        $location_no_root = delete(merge({
-          'vhost'                      => $key,
-          'ssl'                        => $ssl,
-          'location_custom_cfg_append' => $location_custom_cfg_append,
-        }, $location_trimmed), 'fast_cgi_params_extra')
-
-        # If www_root was removed with all the trimmings,
-        # add it back it
-        if ! defined($location_no_root['fastcgi'])
-          or empty($location_no_root['fastcgi'])
-        {
-          $location_merged = merge({
-            'www_root' => $vhost['www_root'],
-          }, $location_no_root)
-        } else {
-          $location_merged = $location_no_root
-        }
-
-        create_resources(nginx::resource::location, { "${lkey}" => $location_merged })
-      }
+      create_resources(nginx::resource::location, { "${lkey}" => $location_merged })
     }
 
     if ! defined(Puphpet::Firewall::Port[$vhost['listen_port']]) {
@@ -203,11 +223,6 @@ if hash_key_equals($nginx_values, 'install', 1) {
 
   if ! defined(Puphpet::Firewall::Port['443']) {
     puphpet::firewall::port { '443': }
-  }
-
-  if count($nginx_values['upstreams']) > 0 {
-    notify { 'Adding upstreams': }
-    create_resources(puphpet::nginx::upstream, $nginx_values['upstreams'])
   }
 
   if defined(File[$puphpet::params::nginx_webroot_location]) {
